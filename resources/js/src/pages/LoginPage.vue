7b64b6ab253977bac6cf7342441e53611b499c09
IgorOlikov <i.olikov@mail.ru>
2024-04-01 06:27:32 +0500

    vue login

--- resources/js/src/pages/LoginPage.vue
+++ resources/js/src/pages/LoginPage.vue
@@ -1,5 +1,16 @@
 <script setup>
 
+import {ref} from "vue";
+import {useAuthStore} from "@/store/AuthStore.js";
+
+const email = ref('');
+const password = ref('');
+
+
+const authStore = useAuthStore();
+
+
+
 </script>
 
 <template>
@@ -7,14 +18,23 @@
     <div class="min-h-screen flex items-center justify-center w-full dark:bg-gray-200">
         <div class="bg-white dark:bg-green-300 shadow-md rounded-lg px-8 py-6 max-w-md">
             <h1 class="text-2xl font-bold text-center mb-4 dark:text-gray-200">Welcome Back!</h1>
+            <h1
+                v-if="authStore.isErrorMessage"
+            >
+                {{ authStore.errorMessage }}
+            </h1>
             <form action="#">
                 <div class="mb-4">
                     <label for="email" class="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">Email Address</label>
-                    <input type="email" id="email" class="shadow-sm rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" placeholder="your@email.com" required>
+                    <input
+                        v-model="email"
+                        type="email" id="email" class="shadow-sm rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" placeholder="your@email.com" required>
                 </div>
                 <div class="mb-4">
                     <label for="password" class="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">Password</label>
-                    <input type="password" id="password" class="shadow-sm rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" placeholder="Enter your password" required>
+                    <input
+                        v-model="password"
+                        type="password" id="password" class="shadow-sm rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" placeholder="Enter your password" required>
                     <a href="#"
                        class="text-xs text-gray-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Forgot
                         Password?</a>
@@ -28,7 +48,9 @@
                        class="text-xs text-indigo-500 hover:text-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Create
                         Account</a>
                 </div>
-                <button  type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Login</button>
+                <button
+                    @click.prevent="authStore.sighIn(email,password)"
+                    type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Login</button>
             </form>
         </div>
     </div>
--- resources/js/src/store/AuthStore.js
+++ resources/js/src/store/AuthStore.js
@@ -1,17 +1,55 @@
 import {defineStore} from "pinia";
 import {computed, ref} from "vue";
+import axios from "axios";
 export const useAuthStore
     = defineStore('authStore', () => {
 
-        const auth = ref(false);
-     const isAuth = computed (() => auth.value )
+    const userInfo = ref([]);
 
-    const login = async () => {
-        auth.value = true;
+    const expiresIn = ref();
+
+    const auth = ref(false);
+
+    const errorMessage = ref('')
+
+    const appDomain = 'http://localhost';
+
+    const apiVersion = '/api/v1';
+
+    const isErrorMessage = computed(() => {
+        return errorMessage.value.length !== 0;
+    })
+
+    const isAuth = computed (() => auth.value )
+
+    const sighIn = async (email, password) => {
+       try {
+           const response = await axios.post(`${appDomain}${apiVersion}/auth/login`,{
+               email: email,
+               password: password
+           });
+
+           localStorage.setItem('token', response.data.access_token)
+           expiresIn.value = response.data.expires_in
+           userInfo.value = response.data.user
+           auth.value = true
+       }catch (err) {
+           errorMessage.value = err.response.data.message
+       }
     }
+    const signUp = async () => {
+
+    }
+
     const logout = async () => {
         auth.value = false;
     }
 
-    return { auth, isAuth, login, logout }
+    const refreshToken = async () => {
+
+    }
+
+
+
+    return { auth, isAuth, sighIn, signUp, logout , errorMessage, isErrorMessage, userInfo, expiresIn}
 });
