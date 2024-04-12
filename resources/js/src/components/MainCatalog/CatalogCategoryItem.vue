5df21241b4e63faf919738fe3b1008d72157faab
IgorOlikov <i.olikov@mail.ru>
2024-04-12 19:17:57 +0500

    main catalog style

--- resources/js/src/components/MainCatalog/CatalogCategoryItem.vue
+++ resources/js/src/components/MainCatalog/CatalogCategoryItem.vue
@@ -15,24 +15,26 @@ defineProps({
 
 <template>
 
-        <div v-if="categories.length !== 0"
-            class="border">
-            <img :src="`${image}`">
-
-            <a :href="`/catalog/category/${slug}`">
+        <div class="flex flex-col rounded-xl transition hover:-translate-y-2">
+            <div
+                v-if="categories.length !== 0"
+                class="mx-auto my-auto"
+            >
+                <a :href="`/catalog/category/${slug}`">
+                    <img  :src="`${image}`">
+                </a>
+            </div>
+            <div
+                v-else
+                class="mx-auto my-auto"
+            >
+                <a :href="`/catalog/category/${slug}/product`">
+                    <img  :src="`${image}`">
+                </a>
+            </div>
+            <div class="mx-auto mt-2">
                 <p>{{ name }}</p>
-            </a>
-
-        </div>
-        <div v-else
-             class="border"
-        >
-            <img :src="`${image}`">
-            <a :href="`/catalog/category/${slug}/product`">
-                <p>{{ name}}</p>
-            </a>
-
-
+            </div>
         </div>
 
 
--- resources/js/src/components/MainCatalog/CatalogCategoryList.vue
+++ resources/js/src/components/MainCatalog/CatalogCategoryList.vue
@@ -12,8 +12,8 @@ defineProps({
 </script>
 
 <template>
-    <div class="flex items-center justify-center max-h-screen py-96">
-        <div class="grid grid-cols-3 grid-rows-2 gap-5 min-h-96 min-w-96 border">
+    <div class="flex items-center justify-center max-h-screen py-96 ">
+        <div class="grid grid-cols-3 grid-rows-2 gap-5 min-h-96 min-w-96">
             <catalog-category-item
 
                 v-for="category in catalog"
